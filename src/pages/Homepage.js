59afbff42d6fa4881c5a4848e0ed7cffa4ae29bc
Nitesh Yadav <niteshnnlp@gmail.com>
2024-02-01 16:24:22 +0530

    first commit

---
+++ src/pages/Homepage.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { useState } from "react"
+import { useEffect } from 'react'
+import './homepage.css';
+import Navbar from '../components/Navbar';
+import { Link } from 'react-router-dom'
+
+
+function Homepage() {
+
+    const [records, setRecords] = useState([])
+
+    useEffect(()=>{
+        fetch('https://api.tvmaze.com/search/shows?q=all')
+        .then(response => response.json())
+        .then(data => setRecords(data))
+        .catch(err => console.log(err))
+    },[])
+  return (
+    <>
+    <Navbar/>
+    <div className='movies'>
+        {console.log(records) }
+       
+        {records.map((list)=>{
+            // console.log(list.show.image)
+            
+           if(list.show.image ){
+            
+            return(
+             <>
+
+                <div className='movie_container' key ={list.show.id} >
+                    
+                    <img src = {list.show.image != null ? list.show.image.medium : null} alt="show image" className='show_image'/>
+                    <p>rating: {list.show.rating.average}</p>
+                    <h4>{list.show.name}</h4>
+                    <button type="button" class="btn btn-secondary">+ Watchlist</button>
+                    <Link to ='details' state={{list: list}}><button type="button" class="btn btn-secondary">Details</button></Link>
+                       
+                </div>
+                </>
+            )
+           }
+            
+        })}
+        
+        
+      
+    </div>
+    </>
+  )
+}
+
+export default Homepage
